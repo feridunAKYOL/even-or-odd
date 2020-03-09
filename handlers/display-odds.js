eba16ff0c471894bc8e4205f1a229df9a9ee7aa3
feridun Akyol <feridunakyol34@gmail.com>
2020-03-09 22:05:49 +0100

    handlers added

--- handlers/display-odds.js
+++ handlers/display-odds.js
@@ -1,10 +1,17 @@
 function oddsHandler() {
-  debugger;
-  // write me!
-
-  // log user interaction: handler name, user input, new state
-  log.push({
-    handler: 'odds',
-    strings: JSON.parse(JSON.stringify(strings))
-  })
+	debugger;
+	// write me!
+	let ulElement = document.getElementById('selected');
+	ulElement.innerHTML = '';
+	let odds = strings.odds;
+	for (let item of odds) {
+		const liElement = document.createElement('li');
+		liElement.textContent = item;
+		ulElement.appendChild(liElement);
+	}
+	// log user interaction: handler name, user input, new state
+	log.push({
+		handler: 'odds',
+		strings: JSON.parse(JSON.stringify(strings))
+	});
 }
--- handlers/save.js
+++ handlers/save.js
@@ -1,11 +1,26 @@
 function saveHandler() {
-  debugger;
-  // write me!
+	debugger;
+	// write me!
+	let input = document.getElementById('input').value;
 
-  // log user interaction: handler name, user input, new state
-  log.push({
-    handler: 'save',
-    newInput,
-    strings: JSON.parse(JSON.stringify(strings))
-  })
+	if (strings.evens.includes(input) || strings.odds.includes(input) || strings.NaNy.includes(input)) {
+		return;
+	}
+	if (input === '') {
+		return;
+	}
+	if (isNaN(input) === true) {
+		strings.NaNy.push(input);
+	} else if (Number(input) % 2 === 1) {
+		strings.odds.push(input);
+	} else {
+		strings.evens.push(input);
+	}
+	document.getElementById('input').reset();
+	// log user interaction: handler name, user input, new state
+	log.push({
+		handler: 'save',
+		newInput,
+		strings: JSON.parse(JSON.stringify(strings))
+	});
 }
